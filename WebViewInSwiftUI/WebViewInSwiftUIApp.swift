d9279c6c4bb02f0075a195733455c81b8aab5fe1
Shireesh Marla <shireesh.marla@swipiicard.com>
2021-12-01 11:49:23 +0000

    Initial Commit

---
+++ WebViewInSwiftUI/WebViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewInSwiftUIApp.swift
+//  WebViewInSwiftUI
+//
+//  Created by Shireesh Marla on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WebViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
